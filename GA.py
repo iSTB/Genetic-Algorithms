d13da3b0d076f0af9be85a62db235f7b1760bf31
jmckayfletcher <jmckayfletcher@ubuntu.(none)>
2016-11-18 17:11:01 +0000

    loads o' stuff

--- GA.py
+++ GA.py
@@ -1,16 +1,36 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib
+import schematax as sx
 
 def all_ones(indv):
     return indv.count('1')
 
+with open('easy20.txt', 'rU') as kfile:
+    lines = kfile.readlines()
+    n = int(lines[0]) # Number of items
+    c = int(lines[n+1]) # Knapsack capacity
+    items = {int(line.split()[0]) : tuple(map(int, line.split()))[1:] for line in lines[1:n+1]} 
 
-
+def k_fit(b):
+    if len(b) != n:
+        print "error"
+        return -1
+    w = 0
+    v = 0
+    for i in range(n):
+        if b[i] =='1':
+            v += items[i+1][0] 
+            w += items[i+1][1]
+    if w >c:
+        v = 0
+      #  v = (1./((w-c)))*v
+    return v
+     
 class GA(object):
 
-    def __init__(self,func = all_ones,n=10,p=16,mu=0.1,s=0,e=False):
+    def __init__(self,func = all_ones,n=15,p=32,mu=0.005,s=0,e=False):
         """
         n: number of indvs
         p: number of bits for each indv
@@ -31,12 +51,15 @@ class GA(object):
         self.best = '' #stores best overall indivdual
         self.bests = [] #stores best individual from each gen
         self.best_f = -float('inf') #the fittness of the best overal indivdual
-
+        self.bests_f = []
         self.av_f = [] #stores the average fitness of each population
-
-
+        self.av_os = []
+        self.av_ds = []
+        self.av_fs = []
+        self.schemas = [] # stores the schematic completion of each gen
+        self.bb = [] #stores the building blocks of each gen
     def init_pop(self):
-        self.pop = [''.join(str(random.choice(['1','0'])) for _ in xrange(self.p)) for _ in xrange(self.p) ]
+        self.pop = [''.join(str(random.choice(['1','0'])) for _ in xrange(self.p)) for _ in xrange(self.n) ]
        
 
     
@@ -45,8 +68,18 @@ class GA(object):
 
 
     def crossover(self,ma,da):
+
+        #new = ''
+        #for i in range(self.p):
+        #    if ma[i] == da[i]:
+        #        new += ma[i]
+        #    else:
+        #        new+= random.choice(['1','0']) 
         pivot = random.randint(0,self.p)
 
+        #return [new]
+
+    
         son = ma[:pivot] + da[pivot:]
         daught = da[:pivot] + ma[pivot:]
 
@@ -70,7 +103,7 @@ class GA(object):
             if  f > bestpf:
                 bestp = indv
                 bestpf = f
-
+        self.bests_f.append(max(self.fs.values()))
         self.bests.append(bestp)
         self.av_f.append(np.mean(self.fs.values()))
 
@@ -87,7 +120,7 @@ class GA(object):
             current += self.fs[indv]
             if current > pick:
                 return indv
-
+        return indv
 
     def select(self):
         if self.s == 0:
@@ -112,20 +145,128 @@ class GA(object):
 
 
 
-    def run(self,steps=100):
-        self.init_pop()
+    def get_bb(self):
 
+        schemata = [s for s in self.schemas[-1] if not(s.is_empty_schema()) and s.get_anti_order() > 0]
+        av_o = np.mean([s.get_order() for s in schemata])
+        av_def = np.mean([s.get_defining_length() for s in schemata])
+        av_fit = np.mean([s.fit for s in schemata])
+        print av_o
+        self.bb.append([s for s in schemata if s.fit >= av_fit and s.get_order() <= av_o and s.get_defining_length() <= av_def])
+        self.av_fs.append(np.mean([s.fit for s in self.bb[-1]]))
+        self.av_os.append(np.mean([s.get_order() for s in self.bb[-1]]))
+        self.av_ds.append(np.mean([s.get_defining_length() for s in self.bb[-1]]))
+    def run(self,steps=200):
+        self.init_pop()
+        self.perc = []
+        self.schemas.append(sx.complete(self.pop, func =self.func))
+        self.get_bb()
 
         for i in range(steps):
             print "gen: ",i
             self.make_next_gen()
-            print self.bests
-            print self.best
+            self.get_bb()
+            #print self.bb
+            self.schemas.append(sx.complete(self.pop, func=self.func))
+            old_s,new_s = self.schemas[-2],self.schemas[-1]
+            
+            old_b, new_b = self.bb[-2], self.bb[-1] 
 
+            bs = blends(old_b)
+            c = 0
+            for s in new_b:
+                if s in bs:
+                    c +=1
+            if len(new_b) == 0:
+                self.perc.append(0)
+            else:
+                self.perc.append((c*1.)/len(new_b))    
 
+            #print self.bests
+            #print self.best
+def blends(schemata):
+    bs = []
+    c = 0
+    for s1 in schemata:
+        for s2 in schemata:
+          #  print bs
+            c +=1
+            blend = ''
+           # print s1,s2
+            if s1.is_empty_schema() or s2.is_empty_schema():
+                continue
+            for i in xrange(len(s1)):
+                if s1[i] == s2[i]: 
+                    blend += s1[i]
+
+                elif s1[i] == '*' and s2[i] != '*':
+                        blend += s2[i]
+                elif s2[i] == '*' and s1[i] != '*':
+                        blend += s1[i]
+            if len(blend) == len(s1):# and blend not in schemata:
+                bs.append(blend)
+    return list(set(bs))
 if __name__ == "__main__":
-    g = GA(e=True) 
-    g.run()
-    plt.plot(g.av_f)
+    x = ['110101', '100100','011000', '110100', '011001', '111111']
+    ss = sx.complete(x)
+
+    print ss
+    
+    bls = blends(ss)
+    sx.draw(bls + [sx.schema()], 'blends')
+    sx.draw(ss, 'complete')
+    
+    
+    
+    
+    
+    
+   # plt.plot(g.av_f)
+   # plt.show()
+   # fs = {}
+    #for s in g.bb[-1]:
+    #    o = s.get_order()
+    #    f = s.fit
+    #    print o,f
+    #    if o not in fs.keys():
+    #        fs[o] = (f,1.)
+    #    else:
+    #        fs[o] = (fs[o][0] + f, fs[o][1] +1.)
+
+   # X = []
+   # Y = []
+   # for order in sorted(fs.keys()):
+   #       X.append(order)
+   #       Y.append(fs[order][0]/fs[order][1]) 
+    
+   # plt.plot(X,Y)
+   # plt.show()
+   # plt.plot(g.av_fs)
+    #os = []
+    #dls = []
+    ps = []
+    for x in range(5):
+        g = GA(e=False) 
+        g.run(100)
+        ps.append(g.perc)
+        #os.append(g.av_os)
+        #dls.append(g.av_ds)
+
+    #av_o = [np.average(col) for col in zip(*os)]
+    #av_d = [np.average(col) for col in zip(*dls)]
+    #o_er = [np.std(col) for col in zip(*os)]
+    #d_er = [np.std(col) for col in zip(*dls)]
+    av_p = [np.average(col) for col in zip(*ps)]
+    p_er = [np.std(col) for col in zip(*ps)]
+    plt.errorbar(range(len(av_p)),av_p,color='black',yerr=p_er )
+    #plt.errorbar(range(len(av_o)),av_o,color='black',yerr=o_er )
+    #plt.errorbar(range(len(av_d)), av_d,color='black',yerr=d_er,ls='--')
+    plt.xlabel('Generation')
+    plt.ylabel('Proportion of Building Blocks combined')
+    matplotlib.rcParams.update({'font.size': 13})
     plt.show()
+             
+
+
+    #print g.schemas
 
--- SA.py
+++ SA.py
@@ -2,15 +2,69 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import schematax as sx
+import struct
+
+
+
+with open('easy20.txt', 'rU') as kfile:
+    lines = kfile.readlines()
+    n = int(lines[0]) # Number of items
+    c = int(lines[n+1]) # Knapsack capacity
+    items = {int(line.split()[0]) : tuple(map(int, line.split()))[1:] for line in lines[1:n+1]} 
+
+
+
+
+def bitsToFloat(b):
+#    s = struct.pack('>l', b)
+    return struct.unpack('d', struct.pack('Q', int(b, 0)))[0]
+    #return struct.unpack('f', b)[0]
+
+def k_fit(b):
+    if len(b) != n:
+        print "error"
+        return -1
+    w = 0
+    v = 0
+    for i in range(n):
+        if b[i] =='1':
+            v += items[i+1][0] 
+            w += items[i+1][1]
+    #print v
+    if w >c:
+        v = (1./((w-c)))*v
+  #  print v
+    return v  
+#print bitsToFloat('0b01000000001010010001011')
+
+
+def booths(indv):
+    
+    x = int('0b' + indv[:int(len(indv)/2.0)],0)
+    y = int('0b' + indv[int(len(indv)/2.0):],0)
+  #  y_bits = struct.pack('>l', int('0b' + indv[int(len(indv)/2.0):],0))
+
+  
+#    x = struct.unpack('>f',x_bits)[0]
+ #   y = struct.unpack('>f',y_bits)[0]
+   
+   # print x,y
+#    print x,y
+   # return -(x**2 + y**2)
+   # z = (x+2*y-7)**2 + (2*x + y -5)**2
+    z = x+y
+    return -z
+
+
+#print booths('10101010101010101010101010101010')
+
 
 def all_ones(indv):
     return indv.count('1')
 
-
-
 class SA(object):
 
-    def __init__(self,func = all_ones,n=10,p=16,mu=0.1,s=0,e=False):
+    def __init__(self,func = k_fit,n=20,p=20,mu=0.00,s=0,e=False):
         """
         n: number of indvs
         p: number of bits for each indv
@@ -31,27 +85,15 @@ class SA(object):
         self.best = '' #stores best overall indivdual
         self.bests = [] #stores best individual from each gen
         self.best_f = -float('inf') #the fittness of the best overal indivdual
-
+        self.bests_f = []
         self.av_f = [] #stores the average fitness of each population
-
+        self.max_f = 0 # stores the best max fitness of the best schema  
 
     def init_pop(self):
-        self.pop = [''.join(str(random.choice(['1','0'])) for _ in xrange(self.p)) for _ in xrange(self.p) ]
+        self.pop = [''.join(str(random.choice(['1','0'])) for _ in xrange(self.p)) for _ in xrange(self.n) ]
        
-
-    
     def mutate(self,indv):
         return ''.join(str(int(not(int(x)))) if random.random() <= self.mu else x for x in indv)
-
-
-    def crossover(self,ma,da):
-        pivot = random.randint(0,self.p)
-
-        son = ma[:pivot] + da[pivot:]
-        daught = da[:pivot] + ma[pivot:]
-
-        return [son,daught]
-
 
     def eval_pop(self):
         self.fs = {}
@@ -62,7 +104,6 @@ class SA(object):
             f = self.func(indv)
             self.fs[indv] = f
 
-
             if f > self.best_f:
                 self.best = indv
                 self.best_f = f
@@ -72,62 +113,114 @@ class SA(object):
                 bestpf = f
 
         self.bests.append(bestp)
+        self.bests_f.append(bestpf)
         self.av_f.append(np.mean(self.fs.values()))
 
-    def roulette_wheel(self):
-        max = sum(self.fs.values())
 
-        pick = random.uniform(0,max)
-
-        current = 0
-
-        for indv in self.fs.keys():
-            current += self.fs[indv]
-            if current > pick:
-                return indv
-
+    #def select sche
 
     def select(self):
-        if self.s == 0:
-            return self.roulette_wheel()
+        #roulete wheel selection
+        av_o = np.mean([s.get_anti_order() for s in self.schemata if not(s.is_empty_schema())])
+        print av_o
+        schemata = [s for s in self.schemata if (not s.is_empty_schema()) and s.get_anti_order() >= np.floor(av_o) ]
+        max_f = max([s.fit for s in schemata])*1.
+        min_f = min([s.fit for s in schemata])
 
 
-    def get_good_schemata(self):
-        meanf = np.mean([s.fit for s in self.schemata])
-        meano = np.mean([s.get_order() for s in selfschemata]))
+      #  print max_f,min_f
+        for s in schemata:
+            if max_f == min_f:
+                s.fit = 0.1
+            else:
+                s.fit = (s.fit - min_f)/(max_f-min_f)
+            #print s.fit
+        self.max_f = sum([s.fit for s in schemata])
+        
+        pick = random.uniform(0,self.max_f)
+        current = 0
 
-        self.good = [s for s in self.schemata if s.get_order() >= meano and s.fit >= meanfit]
+        for indv in schemata:
+            current += indv.fit
+            if current >= pick:
+                return indv
+        return indv     
+   # def get_good_schemata(self):
+   #     meanf = np.mean([s for s in self.schemata])
+   #     meano = np.mean([s.get_order() for s in selfschemata])
+
+    #    self.good = [s for s in self.schemata if s.get_order() >= meano and all_ones(s) >= meanfit]
     
             
 
 
     def make_next_gen(self):
         self.eval_pop()
-        self.schemata = sx.complete(self.pop,func=self.func)
+        self.schemata = sx.complete(self.pop,func = self.func)
         new = []
         if self.e:
             new.append(self.bests[-1])
         while len(new) <= self.n:
             mum = self.select()
             dad = self.select()
+            
+           # o1 = ''
+           # for i in xrange(self.p):
+           #     if mum[i] == '*':
+           #         o1 += random.choice(['1','0'])
+          #      else:
+           #         o1 += mum[i]
+           # new += [self.mutate(o1)]
+            son = ''
+            for i in xrange(self.p):
+                if mum[i] == dad[i] and mum[i] != '*':
+                    son += mum[i]
+                elif mum[i] != dad[i] and dad[i] == '*':
+                    son += mum[i]
+                elif mum[i] != dad[i] and mum[i] == '*':
+                    son += dad[i]
+                else:
+                    son += random.choice(['1','0'])
 
-            new +=  [self.mutate(x) for x in self.crossover(mum,dad)]
+
+            new +=  [self.mutate(son)]
         self.pop = new
 
     def run(self,steps=100):
-        self.init_pop()
-
-
+       # self.init_pop()
         for i in range(steps):
             print "gen: ",i
             self.make_next_gen()
-            print self.bests
+            print self.pop
+          #  print self.bests
             print self.best
 
 
 if __name__ == "__main__":
-    g = GA(e=True) 
-    g.run()
-    plt.plot(g.av_f)
-    plt.show()
+    from GA import GA
+    ss = []
+    gs = []
+    for i in range(10):
+        pop = [''.join(str(random.choice(['1','0'])) for _ in xrange(20)) for _ in xrange(20) ]
+        s = SA(e=True,func=k_fit,n=20,p=20,mu=0.0) 
+        s.pop = pop
+        s.run(100)
+        g = GA(func = k_fit,n=20, p =20,e=True, mu=0.0)
+        g.pop = pop
+        g.run(100)
+        ss.append(s.bests_f)
+        gs.append(g.bests_f)
+        #plt.plot(s.av_f)
+        #plt.plot(g.av_f)
+    #    plt.plot(s.bests_f)
+    #    plt.plot(g.bests_f)
+    #    plt.show()
+   # pass
 
+    ss = reduce(np.add,ss)
+    gs = reduce(np.add,gs)
+    plt.plot(map(lambda x: x/10., ss))
+    plt.plot(map(lambda x: x/10., gs))
+    plt.show()
+#    plt.plot(g.bests_f)
+#    plt.show()
